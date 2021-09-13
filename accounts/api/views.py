fefa61c3807300613e7df87181ec8783d114d78c
slybootslion <slybootslion@vip.qq.com>
2021-09-13 09:01:12 +0800

    install djanao restframework

---
+++ accounts/api/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from accounts.api.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
